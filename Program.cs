8b52bd7b4e95b32bacf3daef4fd8171773ee9525
favead <diablo3_monk@mail.ru>
2021-10-27 21:57:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ class Program
             Console.WriteLine("Incorrect data entry! Odd number of lines required and is at least 3 and be unique ");
         }
         else if (param.Length > 1 && param.Length % 2 != 0)
+        else if (param.Length > 1 && param.Length % 2 != 0 && param.Distinct().Count() == param.Length)
         {
             Console.WriteLine(menu.PrintMenu(param));
         }
