40041c6154c1bb2563b96d7f4685095d87b57b60
favead <diablo3_monk@mail.ru>
2021-10-27 22:26:41 +0300

    FInal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Menu menu = new Menu();
-        string[] param = new string[Environment.GetCommandLineArgs().Length];
+        string[] param = new string[Environment.GetCommandLineArgs().Length - 1];
 
         for (int i = 0; i < Environment.GetCommandLineArgs().Length - 1; i++)
         {
@@ -14,8 +14,8 @@ class Program
         }
 
         if (
-            param.Length > 1 && param.Length % 2 == 0 ||
-           !(param.Distinct().Count() == param.Length) ||
+             (param.Length > 1 && param.Length % 2 == 0) ||
+             !(param.Distinct().Count() == param.Length) ||
             param.Length == 1)
         {
             Console.WriteLine("Incorrect data entry! Odd number of lines required and is at least 3 and be unique ");
